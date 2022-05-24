8cbc21a91fa210c8473730ade513fe41cc006a98
Elynelez <Elinarnyex@gmail.com>
2022-05-23 19:28:39 -0500

    para heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let express = require("express")
 let path = require("path")
 let app = express()
-app.use(express.static(__dirname + "/public"))
-let puerto = process.env.puerto || 3000
 
-app.listen(puerto, function(){console.log("corriendo puerto")})
+app.use(express.static(__dirname + "/public"))
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){console.log("corriendo puerto")})
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname + "/views/home.html"))
 })
